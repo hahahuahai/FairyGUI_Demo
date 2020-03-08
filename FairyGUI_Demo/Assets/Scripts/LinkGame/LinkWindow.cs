0d861b919572bed76d8a645819321b410253f738
huahai <279598616@qq.com>
2020-03-08 14:51:16 +0800

    添加linkgame，用fairygui+c#写的连连看游戏。

---
+++ FairyGUI_Demo/Assets/Scripts/LinkGame/LinkWindow.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class LinkWindow : Window
+{
+    GList LinkList;
+
+    GButton firstClick = new GButton();
+    GButton secondClick = new GButton();
+    bool isFirstClicked = true; // 判断玩家是否第一次点击
+
+    public LinkWindow()
+    {
+
+    }
+
+    protected override void OnInit()
+    {
+        base.OnInit();
+        this.contentPane = UIPackage.CreateObject("LinkGame", "LinkWin").asCom;
+        this.Center();
+
+        LinkList = this.contentPane.GetChild("list").asList;
+        LinkList.itemRenderer = RenderListItem;
+        LinkList.numItems = 12;
+        LinkList.onClickItem.Add(OnLinkListClicked);
+    }
+
+    void RenderListItem(int index, GObject obj)
+    {
+        GButton button = (GButton)obj;
+        button.icon = "Pics/" + "i" + index / 2;
+        button.title = "i" + index / 2;
+    }
+
+    void OnLinkListClicked(EventContext eventContext)
+    {
+        GButton item = (GButton)eventContext.data;
+        if (isFirstClicked)//第一次点击
+        {
+            firstClick = item;
+            isFirstClicked = false;
+            return;
+        }
+        if (!isFirstClicked)//第二次点击
+        {
+            secondClick = item;
+            isFirstClicked = true;
+        }
+        if(firstClick.title == secondClick.title)//两次点击的图片一致
+        {
+            firstClick.visible = false;
+            secondClick.visible = false;
+        }
+
+    }
+}
---
+++ FairyGUI_Demo/Assets/Scripts/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FairyGUI;
+
+public class StartGame : MonoBehaviour
+{
+    Window linkWindow;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        UIPackage.AddPackage("UI/LinkGame");
+
+        linkWindow = new LinkWindow();
+
+        linkWindow.Show();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
